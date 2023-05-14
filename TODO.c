527b5164bbf088e60c04dd89d19b422a206a1abc
ShutAP <41197101+Xurape@users.noreply.github.com>
2023-05-14 23:15:36 +0100

    updated todo

--- TODO.c
+++ TODO.c
@@ -33,7 +33,7 @@
  * 
  * TODO Lista de espera
  * * Verificar lista de espera do medico
- * ! Dado o codigo do medico retirar da lista de espera o prox. utente a ser atendido
+ * * Dado o codigo do medico retirar da lista de espera o prox. utente a ser atendido
  * * Dado o codigo do medico verificar o numero de utentes em lista de espera
  * * Listar todos os utentes ordenados por medico
  * 
